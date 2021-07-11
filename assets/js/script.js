412de3377e003c113e41bddbe76ef03b999fc7c2
Dylan Shoemaker <me@dylanshoemaker.com>
2021-07-10 21:31:24 -0600

    add suedo code

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,36 @@ var resultsEl = document.getElementById("results"); //Correct! or Wrong! to be d
 
 var initialsEl = document.getElementById("initials"); //asks for initials one the last page
 var submitButton = document.getElementById("submit"); //submit button variable
+
+
+function startQuiz() { //what happens when you press the start quiz button
+  //countdown timer is set at 75seconds to begin with
+  //have timer countdown at start of quiz
+  //hide the start page 
+  //unhide first question
+
+  //write out the questions here?
+  // title
+  // choices
+  // submit button
+  //transitional function to next function
+};
+
+
+
+function renamethis() { //what happens when you answer a question
+  //if wrong issue penalty -15 seconds
+  //Correct! or Wrong! use the resultsEl function and have that function display string on document
+
+  //go to next question
+
+  //transitional function to next function
+};
+
+function endQuiz() {
+  //hide questions
+  //unhide final-results
+  //use dom to display final score
+  //text input box for initials 
+  //submit button should transition to high score page
+};
