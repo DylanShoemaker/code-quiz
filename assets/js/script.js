0de29c8c70db299a0c02f981f911aa014e7c474f
Dylan Shoemaker <me@dylanshoemaker.com>
2021-07-11 11:47:16 -0600

    make sure javascript is referencing correct id in html, get rid of p tags in html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 //what question are we on right now?
-var currentQuestionIndex = 0; //starts at 0
+var questionArray = 0; //starts at 0
 var time = questions.length * 15; // 15 seconds per question = 75 seconds total, and the penalty will also be 15 seconds so this might come in handy
-var countdownEl; //this will run the countdown, like a container in html 
+var countdownId; //this will run the countdown, like a container in html 
 
 
 
@@ -14,7 +14,7 @@ var startButton = document.getElementById("start"); //start button variable
 
 var questionsEl = document.getElementById("questions"); //references the different questions with the choices
 var questionTitle = document.getElementById("title"); //references the different questions
-var questionChoices = document.getElementById("choices"); //references the different choices, choices will act like submit button as well
+var questionChoices = document.getElementById("multiple-choice"); //references the different choices, choices will act like submit button as well
 
 var resultsEl = document.getElementById("results"); //Correct! or Wrong! to be displayed on following page with new question
 
@@ -29,14 +29,15 @@ function startQuiz() { //what happens when you press the start quiz button
 
   questionsEl.removeAttribute("class"); // this will display the questions
  
-  countdownEl = setInterval(updatedTime, 1000); //have timer countdown at start of quiz  https://www.w3schools.com/js/js_timing.asp
+  countdownId = setInterval(updatedTime, 1000); //have timer countdown at start of quiz  https://www.w3schools.com/js/js_timing.asp
 
   countDown.textContent = time;  //countdown timer is set at 75seconds to begin with
+
   getQuestions();  //transitional function to next function
 };
 
 function getQuestions() {
-  var currentQuestion = questions[currentQuestionArray]; //reference the questions-array.js
+  var currentQuestion = questions[questionArray]; //reference the questions-array.js
 
   var titleEl = document.getElementById("questiontitle");
   titleEl.textContent = currentQuestion.title;
@@ -45,13 +46,13 @@ function getQuestions() {
   questionChoices.innerHTML = ""; //delete old choices
 
   
-  currentQuestion.choices.forEach(function(choice, i) {  // create a loop to actually add in the new choices
+  currentQuestion.choices.forEach(function(choice, i) {  // create a loop to actually add in the new choices https://stackoverflow.com/questions/56024232/use-the-foreach-function-to-add-option-elements-to-select-html-element    
     //because the old choices are deleted this will add brand new choices into the mix
     var loopChoices = document.createElement("button");
     loopChoices.setAttribute("class", "choice");
     loopChoices.setAttribute("value", choice);
 
-    loopChoices.textContent = i + 1 + ". " + choice;
+    loopChoices.textContent = i + 1 + ". " + choice;   //https://www.youtube.com/watch?v=49pYIMygIcU  its like 40 minutes long and they didn't section it out but its full of good information
 
     
     loopChoices.onclick = onButtonClick; 
@@ -60,14 +61,50 @@ function getQuestions() {
   });
 };
 
-function onButtonClick() { //what happens when you answer a question
-  //if wrong issue penalty -15 seconds
-  //Correct! or Wrong! use the resultsEl function and have that function display string on document
+function onButtonClick() {
+  // check if user guessed wrong
+  if (this.value == questions[questionArray].answer) {
 
-  //go to next question
+    resultsEl.textContent = "Correct!";
 
-  //transitional function to next function
+    
+  } else {
+
+
+    time -= 15; //issue 15 second penalty
+    if (time < 0) { 
+      time = 0;  //this is so that we dont have negative numbers
+    }
+
+    countDown.textContent = time; //updates time on page
+
+    //add in results to this
+    //add in results to this
+    //add in results to this
+
+
+    questionArray++; // this moves the question along!!!!!!
+
+
+
+    resultsEl.textContent = "Wrong!"; // alerts them that they are wrong on the page though, not an alert pop-up
+    if (questionArray === questions.length) {
+      endQuiz();
+    } else {
+      getQuestion();
+    }
+  }
+
+
+
 };
+
+
+function updatedTime() {
+  //this function is for what goes on in the timer, this updated time acts as a container
+};
+
+
 
 function endQuiz() {
   //hide questions
