14e0fce9a823cca48ec4f19f945d7d8f2b425169
Dylan Shoemaker <me@dylanshoemaker.com>
2021-07-11 10:49:45 -0600

    hide strings not in use use display: none in css use startButton.onclick = startQuiz to start the quiz use foreach method to group questions and answers from the second js file reference new high score html in index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,11 @@
+//what question are we on right now?
+var currentQuestionIndex = 0; //starts at 0
+var time = questions.length * 15; // 15 seconds per question = 75 seconds total, and the penalty will also be 15 seconds so this might come in handy
+var countdownEl; //this will run the countdown, like a container in html 
+
+
+
+
 //first lets add some variables and reference them to the html dom elements
 var countDown = document.getElementById("countdown"); //for the countdown timer in the header
 
@@ -14,22 +22,45 @@ var initialsEl = document.getElementById("initials"); //asks for initials one th
 var submitButton = document.getElementById("submit"); //submit button variable
 
 
-function startQuiz() { //what happens when you press the start quiz button
-  //countdown timer is set at 75seconds to begin with
-  //have timer countdown at start of quiz
-  //hide the start page 
-  //unhide first question
 
-  //write out the questions here?
-  // title
-  // choices
-  // submit button
-  //transitional function to next function
+function startQuiz() { //what happens when you press the start quiz button
+  var startScreenEl = document.getElementById("start-screen"); //select the first page html
+  startScreenEl.setAttribute("class", "hide"); // references the css stylesheet, display: none .....so this hides the initial page
+
+  questionsEl.removeAttribute("class"); // this will display the questions
+ 
+  countdownEl = setInterval(updatedTime, 1000); //have timer countdown at start of quiz  https://www.w3schools.com/js/js_timing.asp
+
+  countDown.textContent = time;  //countdown timer is set at 75seconds to begin with
+  getQuestions();  //transitional function to next function
 };
 
+function getQuestions() {
+  var currentQuestion = questions[currentQuestionArray]; //reference the questions-array.js
+
+  var titleEl = document.getElementById("questiontitle");
+  titleEl.textContent = currentQuestion.title;
 
 
-function renamethis() { //what happens when you answer a question
+  questionChoices.innerHTML = ""; //delete old choices
+
+  
+  currentQuestion.choices.forEach(function(choice, i) {  // create a loop to actually add in the new choices
+    //because the old choices are deleted this will add brand new choices into the mix
+    var loopChoices = document.createElement("button");
+    loopChoices.setAttribute("class", "choice");
+    loopChoices.setAttribute("value", choice);
+
+    loopChoices.textContent = i + 1 + ". " + choice;
+
+    
+    loopChoices.onclick = onButtonClick; 
+
+    questionChoices.appendChild(loopChoices); // puts the values on the page
+  });
+};
+
+function onButtonClick() { //what happens when you answer a question
   //if wrong issue penalty -15 seconds
   //Correct! or Wrong! use the resultsEl function and have that function display string on document
 
@@ -45,3 +76,6 @@ function endQuiz() {
   //text input box for initials 
   //submit button should transition to high score page
 };
+
+
+startButton.onclick = startQuiz; //this starts the quiz
